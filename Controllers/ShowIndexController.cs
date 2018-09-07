4bb9f181bcbb8cf02509714cae79ce45d31f03b8
张潇勇 <michael_zhang@hegret.com>
2018-09-07 19:05:19 +0800

    add

---
+++ Controllers/ShowIndexController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WTB.Controllers
+{
+    public class ShowIndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
